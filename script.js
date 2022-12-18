0824d3e68b9294f2f9b71f7e5dbcb193b83d3181
Gabriel <2021240@student.cct.ie>
2022-12-18 19:38:37 +0000

    tasks

--- script.js
+++ script.js
@@ -387,14 +387,41 @@ const gabrielArray = [
 properties 'country', 'capital', 'language', 'population' and 
 'neighbours' (an array like we used in previous assignments*/
 
+// LECTURE: Object Methods
+// 1. Add a method called 'describe' to the 'myCountry' object. This method 
+// will log a string to the console, similar to the string logged in the previous 
+// assignment, but this time using the 'this' keyword.
+
+// 2. Call the 'describe' method
+
+// 3. Add a method called 'checkIsland' to the 'myCountry' object. This 
+// method will set a new property on the object, called 'isIsland'. 
+// 'isIsland' will be true if there are no friends, and false if 
+// there are. Use the ternary operator to set the property.
+
 const myCountry = {
     country: 'Finland',
     capital: 'Helsinki',
     languange: 'finnish',
     population: 6,
-    friends: ['Venezuela', 'Argentina', 'Colombia']
+    friends: ['Venezuela', 'Argentina', 'Colombia'],
+    isIsland: false,
+
+    checkIsland: function() {
+        this.isIsland = this.friends.length ? false : true;
+    },
+
+    describe: function() {
+        return `${this.country} has ${this.population} million ${this.languange} speaking people, ${this.friends.length} neighbouring countries and a capital called ${this.capital}.`;
+
+    }
+
+
+
+
 };
 
+console.log(myCountry.describe());
 console.log(myCountry.capital);
 console.log(myCountry['capital']);
 
@@ -432,7 +459,7 @@ const Gabriel = {
     job: 'student',
     friends: ['Ivan', 'Heloisa', 'Marko'],
     bestFriend: 'Ivan',
-    hasDriverLicense: true,
+    hasDriverLicense: false,
 
 
     giveAge: function() {
@@ -446,6 +473,10 @@ const Gabriel = {
         } else {
             return 'has no drivers license';
         }
+    },
+
+    getSummary: function() {
+        return `${this.firstName} is a ${this.giveAge()} -years old ${this.job} and he has ${this.hasDriverLicense ? 'a' : 'no'} drivers license.`
     }
 };
 
@@ -454,3 +485,5 @@ console.log(Gabriel.giveAge());
 console.log(Gabriel.age);
 
 console.log(`${Gabriel.firstName} is ${Gabriel.giveAge()} years old and ${Gabriel.giveDriverLicense()}.`)
+
+console.log(Gabriel.getSummary());
